7432e615162982190822486c99a12775dd635aa3
GizelleRM <ramirezgizelle318@gmail.com>
2025-03-18 04:56:16 -0600

    frond terminado

--- Users/urls.py
+++ Users/urls.py
@@ -1,9 +1,9 @@
 from . import views
 from django.urls import path
 
-
 urlpatterns = [
-    path('', views.index),
+    path('', views.home, name='home'), 
+    #path('', views.index),
     path('hello/<str:username>',views.hello),
     path('about/', views.about),
     path('projects', views.projects),
